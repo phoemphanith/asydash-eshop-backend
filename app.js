e59e9ef672cc732558a656e8ef4c095a3e721a5a
PHOEM Phanith <61651733+phoemphanith@users.noreply.github.com>
2022-12-12 17:03:05 +0700

    First deploy

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ mongoose
   .then(() => console.log("DB connect successfully"))
   .catch((err) => console.log(err));
 
-app.listen(3000, () =>
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
   console.log(`Server is running on http://localhost:3000${apiUrl}`)
 );
